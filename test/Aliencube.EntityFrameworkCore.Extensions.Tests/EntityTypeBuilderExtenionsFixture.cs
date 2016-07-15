a11c784b660e453581d28c35a10654903bb5124d
Gongdo <gongdo@live.com>
2016-07-15 17:05:21 +0900

    Add SetDefaultStringMaxLength extension and tests

--- test/Aliencube.EntityFrameworkCore.Extensions.Tests/EntityTypeBuilderExtenionsFixture.cs
+++ test/Aliencube.EntityFrameworkCore.Extensions.Tests/EntityTypeBuilderExtenionsFixture.cs
@@ -4,6 +4,10 @@ using Xunit;
 using Moq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Moq.Protected;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Aliencube.EntityFrameworkCore.Extensions.Tests
 {
@@ -41,6 +45,42 @@ namespace Aliencube.EntityFrameworkCore.Extensions.Tests
             mapperMock.Verify(m => m.Map(It.IsAny<EntityTypeBuilder<Foo>>()), Times.Once);
         }
 
+        [Fact]
+        public void SetDefaultStringMaxLength_configures_MaxLength()
+        {
+            IEntityType entityType = null;
+            var options = CreateDbContextOptions<FooContext>();
+            using (var context = new FooContext(options, new FooMapper()))
+            {
+                context.Model.GetAnnotations();
+                entityType = context.Model.FindEntityType(typeof(Foo));
+            }
+
+            var fooName = entityType.FindProperty(nameof(Foo.Name));
+            var tagName = entityType.FindProperty(nameof(Foo.Tag));
+            Assert.Equal(10, fooName.GetMaxLength().Value);
+            Assert.Equal(10, tagName.GetMaxLength().Value);
+        }
+
+        [Fact]
+        public void SetDefaultStringMaxLength_is_not_affected_for_explicit_MaxLength_configurations()
+        {
+            IEntityType entityType = null;
+            var options = CreateDbContextOptions<FooContext>();
+            using (var context = new FooContext(options, new FooMapper()))
+            {
+                context.Model.GetAnnotations();
+                entityType = context.Model.FindEntityType(typeof(Foo));
+            }
+
+            var fooAlreadyMappedByOthers = entityType.FindProperty(nameof(Foo.AlreadyMappedByOthers));
+            var fooAnnotatedString = entityType.FindProperty(nameof(Foo.AnnotatedString));
+            var fooFluentApiString = entityType.FindProperty(nameof(Foo.FluentApiString));
+            Assert.Equal(200, fooAlreadyMappedByOthers.GetMaxLength().Value);
+            Assert.Equal(200, fooAnnotatedString.GetMaxLength().Value);
+            Assert.Equal(200, fooFluentApiString.GetMaxLength().Value);
+        }
+
         private static DbContextOptions CreateDbContextOptions<T>()
              where T : DbContext
         {
@@ -64,13 +104,20 @@ namespace Aliencube.EntityFrameworkCore.Extensions.Tests
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string Tag { get; set; }
+        public string AlreadyMappedByOthers { get; set; }
+        [MaxLength(200)]
+        public string AnnotatedString { get; set; }
+        public string FluentApiString { get; set; }
     }
 
     public class FooMapper : IEntityMapper<Foo>
     {
         public virtual void Map(EntityTypeBuilder<Foo> builder)
         {
+            builder
+                .Property(p => p.FluentApiString)
+                .HasMaxLength(200);
         }
     }
 
@@ -84,9 +131,12 @@ namespace Aliencube.EntityFrameworkCore.Extensions.Tests
         }
 
         public DbSet<Foo> Fooes { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Foo>().Property(m => m.AlreadyMappedByOthers).HasMaxLength(200);
+            modelBuilder.Entity<Foo>().SetDefaultStringMaxLength(10);
             modelBuilder.Entity<Foo>().Map(mapper);
         }
     }
